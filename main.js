be508de6f7b80caa07c24ecb0ce7a94ee3bf241a
rasty <hvozdetskiyyrii@gmail.com>
2024-05-24 11:24:01 +0300

    update

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import "./src/styles/style.css";
+import "./src/styles/scss/main.scss";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import $ from "jquery";
+import {Fancybox} from "@fancyapps/ui";
 
-setupCounter(document.querySelector('#counter'))
+$(document).ready(function () {
+   $("#app").html("Hello World");
+});
+
+Fancybox.bind("[data-fancybox]", {
+   // Your custom options
+});
